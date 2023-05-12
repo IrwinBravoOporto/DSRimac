4707c78bdd271bb13569740e30af13bfdfe3bc24
Irwin <username@example.com>
2023-05-12 10:14:07 -0500

    UIFont Fixes

---
+++ DSRimac/DSRimac/Extensions/Global.swift
@@ -0,0 +1,115 @@
+class Constants {
+    static let fontSizeSmall = 12.0
+    static let fontSizeMedium = 14.0
+    static let fontSizeLarge = 16.0
+    static let fontSizeXLarge = 18.0
+    static let fontSize2XLarge = 20.0
+    static let fontSize3XLarge = 24.0
+    static let fontSize4XLarge = 28.0
+    static let fontSize5XLarge = 32.0
+    static let fontSize6XLarge = 36.0
+    static let fontSize7XLarge = 40.0
+    static let fontSize8XLarge = 48.0
+    static let fontSize9XLarge = 56.0
+    static let fontSize10XLarge = 64.0
+    static let fontSize11XLarge = 72.0
+    static let fontSize12XLarge = 80.0
+
+    static let fontWeightRegular = "400"
+    static let fontWeightMedium = "500"
+    static let fontWeightSemibold = "600"
+    static let fontWeightBold = "700"
+    static let fontWeightDisplay = "display"
+
+    static let textDecorationNone: String = "none"
+    static let textDecorationUnderline: String = "underline"
+    
+    static let fontFamilyPrimary: String = "BR Sonoma"
+    static let fontFamilySecondary: String = "Rimac"
+
+    static let fontStyleNormal: String = "normal"
+    static let fontStyleItalic: String = "italic"
+
+    static let textTransformNone: String = "none"
+    static let textTransformUppercase: String = "uppercase"
+
+    static let blurBackgroundSubtle: String = "blur(5)"
+    static let blurBackgroundModerate: String = "blur(8)"
+
+    static let blurLayerSubtle: String = "blur(60)"
+    static let blurLayerModerate: String = "blur(120)"
+    static let blurLayerMedium: String = "blur(160)"
+    static let blurLayerStrong: String = "blur(210)"
+
+    static let lineHeightSmall = 12.0
+    static let lineHeightMedium = 16.0
+    static let lineHeightLarge = 20.0
+    static let lineHeightXLarge = 24.0
+    static let lineHeight2XLarge = 28.0
+    static let lineHeight3XLarge = 32.0
+    static let lineHeight4XLarge = 36.0
+    static let lineHeight5XLarge = 40.0
+    static let lineHeight6XLarge = 44.0
+    static let lineHeight7XLarge = 48.0
+    static let lineHeight8XLarge = 56.0
+    static let lineHeight9XLarge = 64.0
+    static let lineHeight10XLarge = 72.0
+    static let lineHeight11XLarge = 96.0
+    static let lineHeight12XLarge = 128.0
+
+    static let letterSpacingSmall = -1
+    static let letterSpacingMedium = 0
+    static let letterSpacingLarge = 0.1
+    static let letterSpacingXLarge = 0.2
+    static let letterSpacing2XLarge = 0.4
+    static let letterSpacing3XLarge = 1
+
+    static let spacingSmall = 4
+    static let spacingMedium = 8
+    static let spacingLarge = 12
+    static let spacingXLarge = 16
+    static let spacing2XLarge = 20
+    static let spacing3XLarge = 24
+    static let spacing4XLarge = 32
+    static let spacing5XLarge = 40
+    static let spacing6XLarge = 48
+    static let spacing7XLarge = 56
+    static let spacing8XLarge = 64
+    static let spacing9XLarge = 80
+    static let spacing10XLarge = 96
+    static let spacing11XLarge = 112
+
+    static let borderRadiusSquare = 0
+    static let borderRadiusSmall = 6
+    static let borderRadiusMedium = 8
+    static let borderRadiusLarge = 16
+    static let borderRadiusXLarge = 24
+    static let borderRadius2XLarge = 40
+    static let borderRadiusPill = 120
+    static let borderRadiusCircle = "50%"
+
+    static let borderWidthNone = 0
+    static let borderWidthThin = 1
+    static let borderWidthMedium = 2
+    static let borderWidthThick = 3
+
+    static let opacitySubtle = 0.1
+    static let opacityModerate = 0.2
+    static let opacityMedium = 0.4
+    static let opacityStrong = 0.6
+    static let opacityExtreme = 0.84
+
+    static let sizeGraphicSmall = 12
+    static let sizeGraphicMedium = 16
+    static let sizeGraphicLarge = 20
+    static let sizeGraphicXLarge = 24
+    static let sizeGraphic2XLarge = 32
+    static let sizeGraphic3XLarge = 48
+    static let sizeGraphic4XLarge = 56
+    static let sizeGraphic5XLarge = 64
+    static let sizeGraphic6XLarge = 72
+    static let sizeGraphic7XLarge = 80
+    static let sizeGraphic8XLarge = 88
+    static let sizeGraphic9XLarge = 96
+
+}
